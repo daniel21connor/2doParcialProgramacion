c6ed8467e5b71cd18b07507a504ea03706ae42a7
daniel21connor <jconnorr1@miumg.edu.gt>
2022-05-01 13:04:39 +0200

    Se agregaron 3 metodos mas, cuales son el nombre del usuario, la tasa de interes y el numero de tarjeta, todo estos metodos, son establecidos en TarjetaCredito

--- TarjetaCredito.java
+++ TarjetaCredito.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package terminaldetarjetas;
 
-/**
- *
- * @author PC GAMING
- */
+
 public class TarjetaCredito {
+       protected int limiteDeTarjetaCredito;
+    protected int saldoCredito;
+  
+
+       TarjetaCredito (){
+           this.limiteDeTarjetaCredito=10000;
+           this.saldoCredito=5000;       
+     
+}
+    
+    public int usuarioCredito (String usuario){
+    System.out.println("EL Nombre Del Usuario ES "+ usuario);
+    
+    
+    
+return 0;    
+}
+       
+public int tasaDeInteres (int capital, int interes, int tiempo){
+    double porcentaje = interes * 0.01;
+    double porcen= capital * porcentaje;
+    double porcentaje3= porcen * tiempo;
+    double tasaTotal= capital *porcentaje3;
+    
+    System.out.print("La Tasa De interes Es De "+"Q"+tasaTotal);
+    return 0;
+}
+    
+  
+    public int numeroDeTarjeta(int numero){
+        System.out.println("El numero De Tarjeta Es  "+numero);
+    
+    return 0;
+}
+    
     
 }
