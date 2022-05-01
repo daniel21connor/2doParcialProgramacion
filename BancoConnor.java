f2e16e59554b01a9434f0576275ffee60e51e49f
daniel21connor <jconnorr1@miumg.edu.gt>
2022-05-01 14:07:31 +0200

    se agrego la primera parte del menu del terminal, en comentarios en el programa se explicara cosas que pasan en la herencia

--- BancoConnor.java
+++ BancoConnor.java
@@ -13,7 +13,7 @@ public class BancoConnor {// llamada de objetos, esta se heredara la tarjeta deb
         case 1:
              System.out.println("Ingrese Su nombre ");
              String usuario=scanner.nextLine();
-             System.out.print(objeto.usuarioCredito(usuario));
+             System.out.print(objeto.usuarioCredito(usuario)); //se llama a lasclase TarjetaCredito
             break;
        case 2:
              System.out.println("ingrese el numero de tarjeta");
@@ -47,10 +47,48 @@ public class BancoConnor {// llamada de objetos, esta se heredara la tarjeta deb
             
             default : System.out.println("GRACIAS POR USAR LA TARJETA");
             
-    }
+    }//nos damos cuenta que todo resultado se da en la pantall principal, no siendo la main.
         return 0;
  }
     
     
-    
+          public int tarjetaDebito (int numer){
+ System.out.println("Bienvenido al banco");       
+    System.out.println("Tarjetas De Debito ");
+    switch (numer){
+        
+        case 1:
+             System.out.println("Ingrese Su nombre ");
+             String usuario=scanner.nextLine();
+             System.out.print(tarjeta.usuarioCredito(usuario));//se llama a la clase TarjetaDebito
+            break;
+        case 2:
+             System.out.println("ingrese el numero de tarjeta");
+             int numero=scanner.nextInt();
+             System.out.println(tarjeta.numeroDeTarjeta(numero));
+             break;
+               case 3:
+             System.out.println("El saldo Actual Es De  " + "Q"+ tarjeta.getSaldo());
+              break;
+         case 4:
+             System.out.println("ingrese el valor del Deposito ");
+             int pago=scanner.nextInt();
+              System.out.print(tarjeta.deposito(pago));
+              break;
+         case 5:
+             System.out.println("ingrese el valor de compra ");
+             int compra=scanner.nextInt();
+             System.out.print(tarjeta.compra(compra));     
+             break;
+        case 6:
+             System.out.println("El limite de credito de la tarjeta es "+ "Q"+tarjeta.getLimiteDebito());
+              break;
+          
+        default : System.out.println("GRACIAS POR USAR LA TARJETA");
+            
+    }
+        return 0;//nos damos cuenta que todo resultado se da en la pantall principal, no siendo la main.
+
+
+}
 }
--- TerminalDeTarjetas.java
+++ TerminalDeTarjetas.java
@@ -1,10 +1,33 @@
  
 package terminaldetarjetas;
 
-public class TerminalDeTarjetas {
+public class TerminalDeTarjetas extends BancoConnor{
 
     
     public static void main(String[] args) {
-            }
+         BancoConnor hola=new BancoConnor();
+ System.out.println( "1" +"  Tarjeta De Credito ");    
+ System.out.println( "2" +"  Tarjeta De Debito ");  
+ System.out.print("\n Escoge la Tarjeta ");
+        int opcion = scanner.nextInt();
+  
+      switch(opcion){
+               
+         case 1 -> { 
+             System.out.println( "1"+" Nombre De la Tarjeta");
+             System.out.println( "2"+" Numero De la Tarjeta");
+             System.out.println( "3"+" Tasa De Interes");
+             System.out.println( "4"+" Saldo");
+             System.out.println( "5"+" Compra");
+             System.out.println( "6"+" Pago");
+             System.out.println( "7"+" limite De Credito");
+             System.out.println( "8"+" Salir");
+                System.out.println( "INGRESA EL NUMERO DE LA OPCION QUE DESEAS");
+                int numer=scanner.nextInt();
+             System.out.println ( hola.tarjetaCredito(numer));
+               }   
+    
+    
+    }
     
 }
