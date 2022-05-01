26f150e45ec699e780fbc2b75743ce8232bd369d
daniel21connor <jconnorr1@miumg.edu.gt>
2022-05-01 13:56:50 +0200

    se agregaron objetos para llamar, atributos y metodos operativos de las tarjeta debito y credito

---
+++ BancoConnor.java
@@ -0,0 +1,7 @@
+package terminaldetarjetas;
+import java.util.Scanner;
+public class BancoConnor {
+       TarjetaCredito objeto = new TarjetaCredito();
+    TarjetaDebito tarjeta= new TarjetaDebito();
+    static  Scanner scanner=new Scanner (System.in);
+}
