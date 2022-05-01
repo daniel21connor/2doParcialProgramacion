80da1be90f57830e1c153749346e4d7ab0d3f5df
daniel21connor <jconnorr1@miumg.edu.gt>
2022-05-01 14:10:22 +0200

    se agrego la ultima parte del menu del programa del main, se daran las funciones de todas las operaciones que permiten las tarjetas y el banco

--- TerminalDeTarjetas.java
+++ TerminalDeTarjetas.java
@@ -5,11 +5,11 @@ public class TerminalDeTarjetas extends BancoConnor{
 
     
     public static void main(String[] args) {
-         BancoConnor hola=new BancoConnor();
+         BancoConnor hola=new BancoConnor(); // nos damos cuenta que heredo las funciones de BancoConnor 
  System.out.println( "1" +"  Tarjeta De Credito ");    
  System.out.println( "2" +"  Tarjeta De Debito ");  
  System.out.print("\n Escoge la Tarjeta ");
-        int opcion = scanner.nextInt();
+        int opcion = scanner.nextInt(); // vemos que podemos usar el Scanner, sin necesidad de tener su  libreria, ya que BancoConnor esta pasando esa herencia
   
       switch(opcion){
                
@@ -26,8 +26,26 @@ public class TerminalDeTarjetas extends BancoConnor{
                 int numer=scanner.nextInt();
              System.out.println ( hola.tarjetaCredito(numer));
                }   
-    
+     case 2 -> {
+            System.out.println( "1"+" Nombre De la Tarjeta");
+             System.out.println( "2"+" Numero De la Tarjeta");
+             System.out.println( "3"+" Saldo");
+             System.out.println( "4"+" Deposito");
+             System.out.println( "5"+" compra");
+             System.out.println( "6"+" limite De tarjeta De debito");
+             System.out.println( "7"+" Salir");
+                System.out.println( "INGRESA EL NUMERO DE LA OPCION QUE DESEAS");
+                int numer=scanner.nextInt();
+             System.out.println ( hola.tarjetaDebito(numer));
+               }
+            
+        
+          
+               }  
+     
+         }  
     
     }
+      
     
-}
+
